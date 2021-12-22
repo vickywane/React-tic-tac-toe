be332b887ddcc20f9973bb9366561a3ee2a2dfd6
Victory <vickywane@gmail.com>
2021-12-22 23:14:17 +0100

    update: created helper file with jsdocs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,20 @@
+/**
+ * Helper file
+ * @module index.js
+ * 
+ * @author Nwani Victory
+ * @description Helper file containing exported functions used in the Game component 
+ */
+
+/**
+ * Generates a random number between 0 - 9
+ * @returns Number
+ */
 const guessNumber = () => Math.floor(Math.random() * 9)
 
+/**
+ * @enum {Array.<Array>} combination of possible matching patterns within a game. 
+ */
 const GAME_WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -11,6 +26,11 @@ const GAME_WINNING_COMBINATIONS = [
     [2, 4, 6]
 ]
 
+/**
+ * 
+ * @param Array exisitingArr finds a random number between 0 - 9 not in exisitingArr agument. 
+ * @returns Number
+ */
 export const findUniqueRandomNumber = (exisitingArr) => {
     let rand = guessNumber();
 
@@ -23,6 +43,11 @@ export const findUniqueRandomNumber = (exisitingArr) => {
     }
 }
 
+/**
+ * 
+ * @param {Array} existingArray finds matching patterns within existingArray argument 
+ * @returns {Object}
+ */
 export const getWinner = (existingArray) => {
     for (let i = 0; i <= 7; i++) {
         const winCombination = GAME_WINNING_COMBINATIONS[i];
